586a277f5e6efa83c978d4e2c5a499df12b48856
Ken Sodemann <ken@ionic.io>
2017-08-25 16:28:50 -0500

    WIP - add a test for core

---
+++ src/compiler/app/test/app-core.spec.ts
@@ -0,0 +1,27 @@
+import * as core from '../app-core';
+
+describe('app-core', () => {
+  it('exists', () => {
+    expect(core).toBeTruthy();
+  });
+
+  describe('generateCore', () => {
+
+  });
+
+  describe('generateCoreES5WithPolyfills', () => {
+
+  });
+
+  describe('getAppFileName', () => {
+
+  });
+
+  describe('getAppPublicPath', () => {
+
+  });
+
+  describe('wrapCoreJs', () => {
+
+  });
+});
